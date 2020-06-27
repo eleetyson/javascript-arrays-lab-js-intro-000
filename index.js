ca081e1bb274f0bfffdd0359f4b1dc284815e3ea
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-27 13:01:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.unshift(name);
+}
